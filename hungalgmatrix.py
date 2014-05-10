7feba2837346d0b202e8535db5023612386a8ba5
TimonKnigge <timonknigge@live.nl>
2014-05-10 12:53:54 +0200

    Ported most of the matrix manipulation code to hungalgmatrix .

--- hungalgmatrix.py
+++ hungalgmatrix.py
@@ -1,8 +1,8 @@
 # Reduces rows individually.
 # Note that this is *not* the same as conventional row reduction in linear algebra.
-def rowReduce(matrix):
+def rowreduce(matrix):
     for row in matrix:
-        # find the minimum in the list
+        # find the minimum in the row
         minimum = row[0]
         for element in row:
             if(element < minimum):
@@ -12,4 +12,16 @@ def rowReduce(matrix):
         for i in range(0, len(row)):
             row[i] -= minimum
 
-    return matrix
+#transpose the matrix
+def transpose(matrix):
+    for x in range(0, len(matrix)):
+        for y in range(x + 1, len(matrix)):
+            matrix[x][y],matrix[y][x] = matrix[y][x],matrix[x][y]
+
+
+# print the matrix
+# (useful for debugging)
+def printmatrix(matrix, offset):    
+    for row in matrix:
+        formatted = [str(element).rjust(offset) for element in row]
+        print(" ".join(formatted))
