d0baeddce1642abdf6582bbd81458a446bb098d5
TimonKnigge <timonknigge@live.nl>
2014-05-10 15:36:10 +0200

    Finished main algorithm loop.

--- hungalgmatrix.py
+++ hungalgmatrix.py
@@ -9,17 +9,20 @@ class solver:
         # create 2 nxn matrices
         self.zeroes = [[0 for y in range(0, len(self.mat))]
                           for x in range(0, len(self.mat))]
-        self.lines = [[False for y in range(0, len(self.mat))]
-                             for x in range(0, len(self.mat))]
+        self.lines = copy.deepcopy(self.zeroes)
+
+        # prepare the output list
+        self.output = [0 for x in range(0, len(self.mat))]
+        self.inuse = [False for x in range(0, len(self.mat))]
 
     def minsum(self):
-                
+        # reduce rows and columns 
+        self.rowreduce()
+        self.transpose()
+        self.rowreduce() # after transposing this reduces the columns
+        self.transpose() # and transpose back
+        
         while(True):
-            # reduce rows and columns 
-            self.rowreduce()
-            self.transpose()
-            self.rowreduce() # after transposing this reduces the columns
-            self.transpose() # and transpose back
 
             # loop through the zeroes matrix, and for each element
             # that is zero, scan horizontally and vertically for
@@ -28,18 +31,85 @@ class solver:
                 for y in range(0, len(self.mat)):
                     if(self.mat[x][y] == 0):
                         self.zeroes[x][y] = self.dirmax(x, y)
-
-            # todo: comment
-            
-            
-
+                    else:
+                        self.zeroes[x][y] = 0
+                        
             print()
             print("Reduced matrix")
-            self.printmatrix(self.zeroes, 2)
+            self.printmatrix(self.mat, 3)
             print()
             print("Zeroes matrix")
-            self.printmatrix(self.mat, 3)
-            
+            self.printmatrix(self.zeroes, 2)
+
+            # reset lines counter
+            lines = 0
+
+            # todo: comment
+            for x in range(0, len(self.mat)):
+                for y in range(0, len(self.mat)):
+                    # clear according to sign
+                    if(self.zeroes[x][y] > 0):
+                        lines += 1
+                        for i in range(0, len(self.mat)):
+                            if(self.zeroes[i][y] > 0):
+                                self.zeroes[i][y] = 0
+                            self.lines[i][y] += 1
+                    elif(self.zeroes[x][y] < 0):
+                        lines += 1
+                        for i in range(0, len(self.mat)):
+                            if(self.zeroes[x][i] < 0):
+                                self.zeroes[x][i] = 0
+                            self.lines[x][i] += 1
+
+            print()
+            print("Lines matrix")
+            self.printmatrix(self.lines, 2)
+            print()
+            print(str(lines)+" lines")
+
+            # If we covered all elements (by using as many lines as there are rows),
+            # we are done. Otherwise, more reduction is necessary.
+            if(lines >= len(self.mat)):
+                break
+
+            # loop through the matrix to find the lowest, non-struck value
+            minvalue,found = 0,False
+            for x in range(0, len(self.mat)):
+                for y in range(0, len(self.mat)):
+                    if(self.lines[x][y] == 0):
+                        minvalue = (self.mat[x][y] if ((minvalue > self.mat[x][y]) or not found) else minvalue)
+                        found = True
+
+            print("lowest: "+str(minvalue))
+
+            # loop through the matrix one more time
+            for x in range(0, len(self.mat)):
+                for y in range(0, len(self.mat)):
+                    if(self.lines[x][y] == 0):
+                        self.mat[x][y] -= minvalue
+                    elif(self.lines[x][y] == 2):
+                        self.mat[x][y] += minvalue
+
+        # Find the optimal permutation
+        self.findpermutation(0)
+        return self.output
+        
+        #end of minsum()
+
+    #  todo comment
+    def findpermutation(self, row):
+        if(row >= len(self.mat)): # done
+            return True
+
+        # loop through the existing columns
+        for i in range(0, len(self.mat)):
+            if((self.mat[row][i] == 0) and not self.inuse[i]):
+                self.output[row] = i
+                self.inuse[i] = True
+                if(self.findpermutation(row + 1)):
+                    return True
+                self.inuse[i] = False
+        return False
     
     # Reduces rows individually.
     # Note that this is *not* the same as conventional row reduction in linear algebra.
